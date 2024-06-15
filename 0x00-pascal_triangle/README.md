3d87a6a52ee9aa59f3a2e8aef3b246b627bf47e4
johntymuriithi <muriithijohn634@gmail.com>
2024-06-15 21:47:01 +0300

    did the pascals DS

---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,1 @@
+Doing pascals over here
---
+++ 0x00-pascal_triangle/testing.py
@@ -0,0 +1,9 @@
+def tester(numbers):
+    for i in range(1, 1):
+        print("Hello world")
+        exit(0)
+
+    print("John jon")
+
+
+tester([20, 30, 40, 50, 47])
